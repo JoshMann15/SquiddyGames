2ebd24f57b35afd76251caabae124f59cdccadd9
Juice <72855553+JoshMann15@users.noreply.github.com>
2023-10-10 14:24:14 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// Load Node modules
 var express = require('express');
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static('public'));
-
+// Port website will run on
+app.listen(8080);
